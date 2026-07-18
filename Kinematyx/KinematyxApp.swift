f01b7191e65c0efad6323fafcdd915d4c611f647
Aakarsh Kachalia <aakarsh.kachalia10@gmail.com>
2026-07-18 12:24:33 -0400

    Initial Commit

---
+++ Kinematyx/KinematyxApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinematyxApp.swift
+//  Kinematyx
+//
+//  Created by Aakarsh Kachalia on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct KinematyxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
